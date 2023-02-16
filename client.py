5ccca33820ca2822113745c9d2e99cca6cdaf0a2
FFFFaaatte <98740260+FFFFaaatte@users.noreply.github.com>
2023-02-17 00:06:18 +0330

    Update client.py

--- client.py
+++ client.py
@@ -1,42 +1,43 @@
 import socket
+import time
 
 IP = 'localhost'
-BUFFSIZE = 1024
+BUFF_SIZE = 1024
 
 
 class Client:
     def __init__(self):
         self.udp_socket = None
         self.xclient_addr = None
-        self.messages = ["Sample Message 1.",
-                         "Sample Message 2.", "Sample Message 3."]
+        self.messages = ["Sample Message 1.", "Sample Message 2.", "Sample Message 3."]
 
     def bind_udp(self):
         self.udp_socket = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.udp_socket.bind((IP, 0))
+        self.udp_socket.bind((IP, 57078))
         udp_socket_name = self.udp_socket.getsockname()[1]
         print(f"Listening UDP on {IP}:{udp_socket_name}")
 
     def get_xclient_init_message(self):
-        _, addr = self.udp_socket.recvfrom(BUFFSIZE)
+        _, addr = self.udp_socket.recvfrom(BUFF_SIZE)
         self.xclient_addr = addr
 
     def send_sample_packets(self):
         for message in self.messages:
             self.udp_socket.sendto(message.encode(), self.xclient_addr)
-            print(f"Sent '{message}'.")
+            print(f"Sent message '{message}'.")
 
     def receive_responses(self):
         for _ in self.messages:
-            message, _ = self.udp_socket.recvfrom(BUFFSIZE)
-            print(f"Received: '{message.decode()}'")
+            message, _ = self.udp_socket.recvfrom(BUFF_SIZE)
+            print(f"Received message '{message.decode()}'")
 
     def start(self):
         self.bind_udp()
         self.get_xclient_init_message()
-        self.send_sample_packets()
-        self.receive_responses()
+        if input() == "start":
+            self.send_sample_packets()
+            self.receive_responses()
 
 
 if __name__ == "__main__":
