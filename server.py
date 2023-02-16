febfe5e0e67843a492fdca99a24563f7e5118f11
FFFFaaatte <98740260+FFFFaaatte@users.noreply.github.com>
2023-02-17 00:06:54 +0330

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket
-from utils import parse_message
 
 IP = 'localhost'
 BUFFSIZE = 1024
@@ -12,20 +11,21 @@ class Server:
     def bind_udp(self):
         self.udp_socket = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.udp_socket.bind((IP, 0))
+        self.udp_socket.bind((IP, 57013))
         udp_socket_name = self.udp_socket.getsockname()[1]
         print(f"Listening UDP on {IP}:{udp_socket_name}")
 
     @staticmethod
     def build_response(message):
-        return message.capitalize() * 2
+        return message.upper()
 
     def handle_requests(self):
         while True:
             message, addr = self.udp_socket.recvfrom(BUFFSIZE)
-            _, payload = parse_message(message.decode())
-            response = self.build_response(payload)
+            print(f"Received message '{message}'.")
+            response = self.build_response(message.decode())
             self.udp_socket.sendto(response.encode(), addr)
+            print(f"Sent message '{response}'.")
 
     def start(self):
         self.bind_udp()
